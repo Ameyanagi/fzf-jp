cee5f9733a7ae1ae235b53109387231244fc24d7
Ameyanagi <ryuichi@ameyanagi.com>
2023-12-22 10:16:02 -0500

    update jp

--- src/pattern.go
+++ src/pattern.go
@@ -26,6 +26,7 @@ const (
 	termPrefix
 	termSuffix
 	termEqual
+    termMigemo
 )
 
 type term struct {
@@ -161,7 +162,8 @@ func BuildPattern(fuzzy bool, fuzzyAlgo algo.Algo, extended bool, caseMode Case,
 	ptr.procFun[termExact] = algo.ExactMatchNaive
 	ptr.procFun[termPrefix] = algo.PrefixMatch
 	ptr.procFun[termSuffix] = algo.SuffixMatch
-
+    ptr.procFun[termMigemo] = algo.Migemo
+    
 	_patternCache[asString] = ptr
 	return ptr
 }
@@ -203,6 +205,12 @@ func parseTerms(fuzzy bool, caseMode Case, normalize bool, str string) []termSet
 		if text != "$" && strings.HasSuffix(text, "$") {
 			typ = termSuffix
 			text = text[:len(text)-1]
+		}
+
+        
+		if strings.HasPrefix(text, "/") {
+			typ = termMigemo
+			text = text[1:]
 		}
 
 		if strings.HasPrefix(text, "'") {
